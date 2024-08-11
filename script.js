a1cbf4364193bf5f3009904cef4e313ab118dfeb
elisakurati <elisakura1811@gmail.com>
2024-08-11 11:24:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navList = document.querySelector('.nav-list');
+
+    menuToggle.addEventListener('click', function() {
+        navList.classList.toggle('active');
+    });
+
     console.log('Página carregada!');
 });
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "img/menu-white.svg";
+    } else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "img/close-white.svg";
+    }
+}
