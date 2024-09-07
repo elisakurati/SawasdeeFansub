5de06ed7a5b511821c6ce14316ce2355627be58a
elisakurati <elisakura1811@gmail.com>
2024-09-07 10:41:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navList = document.querySelector('.nav-list');
+    const menuToggle = document.querySelector('.mobile-menu-icon button'); // Ajuste o seletor se necessário
+    const mobileMenu = document.querySelector('.mobile-menu'); // Ajuste o seletor se necessário
+    const icon = document.querySelector('.mobile-menu-icon .icon'); // Atualiza o ícone do menu
 
     menuToggle.addEventListener('click', function() {
-        navList.classList.toggle('active');
+        if (mobileMenu.classList.contains('open')) {
+            mobileMenu.classList.remove('open'); // Remove a classe 'open' para ocultar o menu
+            icon.src = "img/menu-white.svg"; // Ícone para menu fechado
+        } else {
+            mobileMenu.classList.add('open'); // Adiciona a classe 'open' para mostrar o menu
+            icon.src = "img/close-white.svg"; // Ícone para menu aberto
+        }
     });
 
     console.log('Página carregada!');
 });
-
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "img/menu-white.svg";
-    } else{
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "img/close-white.svg";
-    }
-}
